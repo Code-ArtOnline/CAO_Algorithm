79ee233b4fb5594f242470f703c500d4a4e6a902
ChanLee_KR <110769195+LC-02s@users.noreply.github.com>
2024-02-21 02:43:49 +0900

    Delete 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.js

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.js
+++
@@ -1,5 +0,0 @@
-const input = require('fs').readFileSync('/dev/stdin')
-    .toString().trim().split('\n').map(Number);
-const [ n, ...targetArr ] = input;
-
-console.log(targetArr.sort((a, b) => a - b).join('\n'));
