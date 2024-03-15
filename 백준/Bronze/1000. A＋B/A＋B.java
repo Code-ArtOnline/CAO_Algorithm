1ad02804dd897e7785e245bb5ffd035166bad1e1
toni <144209738+saokiritoni@users.noreply.github.com>
2024-03-15 14:56:34 +0900

    [Bronze V] Title: A+B, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,18 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int a,b;
-        a = sc.nextInt();
-        b = sc.nextInt();
-        System.out.println(a+b);
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(a+b);
+	}
+
 }
