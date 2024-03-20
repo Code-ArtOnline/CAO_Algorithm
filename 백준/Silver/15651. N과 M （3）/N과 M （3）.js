96fcee026963eb42818e8bb97c9b7d3852be0a28
ChanLee_KR <chanlee1007@naver.com>
2024-03-20 13:26:50 +0900

    [Silver III] Title: N과 M (3), Time: 900 ms, Memory: 152180 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -0,0 +1,25 @@
+const input = require('fs').readFileSync(0, 'utf-8')
+    .toString().trim().split(/\s/).map(Number);
+const [ n, m ] = input;
+
+const intArr = Array.from({ length: n }, (_, i) => i + 1);
+const visited = new Array(n).fill(false);
+const selected = [];
+
+const answer = [];
+const dfs = (arr, depth) => {
+    if (depth === m) {
+        const result = selected.map((i) => arr[i]).join(' ');
+        answer.push(result);
+        return;
+    }
+    for (let i = 0; i < arr.length; i += 1) {
+        selected.push(i);
+        visited[i] = true;
+        dfs(arr, depth + 1);
+        selected.pop();
+        visited[i] = false;
+    }
+}
+dfs(intArr, 0);
+console.log(answer.join('\n'));
