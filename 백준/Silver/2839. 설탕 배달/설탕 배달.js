f38d586dd6e1b16c2e5ed4d796e4461f16dc54a8
ChanLee_KR <chanlee1007@naver.com>
2024-04-01 13:56:34 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync('/dev/stdin').toString().trim());
+let cnt = 0;
+let flag = false;
+
+while (n >= 0) {
+    if (n === 0 || n % 5 === 0) {
+        cnt += parseInt(n / 5);
+        console.log(cnt);
+        flag = true;
+        break;
+    }
+    n -= 3;
+    cnt += 1;
+}
+if (!flag) console.log(-1);
