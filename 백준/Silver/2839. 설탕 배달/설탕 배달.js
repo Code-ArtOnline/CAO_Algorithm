c44ee6b09a4c64424513a0f22c8c02204289f80e
ChanLee_KR <chanlee1007@naver.com>
2024-04-01 13:58:11 +0900

    Update 설탕 배달.js

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
-let n = Number(fs.readFileSync('/dev/stdin').toString().trim());
-let cnt = 0;
-let flag = false;
+const n = Number(fs.readFileSync('/dev/stdin').toString().trim());
 
-while (n >= 0) {
-    if (n === 0 || n % 5 === 0) {
-        cnt += parseInt(n / 5);
-        console.log(cnt);
-        flag = true;
-        break;
-    }
-    n -= 3;
-    cnt += 1;
+function calcDelivery(n) {
+    const deliverySteps = (n, cnt) => {
+        if (n === 0 || n % 5 === 0) {
+            return cnt + parseInt(n / 5);
+        }
+        if (n < 0) return undefined;
+        return deliverySteps(n - 3, cnt + 1);
+    };
+
+    return deliverySteps(n, 0) ?? -1;
 }
-if (!flag) console.log(-1);
+
+const result = calcDelivery(n);
+console.log(result);
